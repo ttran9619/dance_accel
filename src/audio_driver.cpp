38b0adfd67fa6351b3bc07ed03b624a1bad10a77
Unknown <lhb@iastate.edu>
2018-03-24 00:41:17 -0500

    Added Test Code to Audio Driver
    Added Test code to audio driver

--- src/audio_driver.cpp
+++ src/audio_driver.cpp
@@ -6,7 +6,7 @@
 /*-----------------------------------------------------------------
 -                           Includes
 -----------------------------------------------------------------*/
-
+#include "AudioFile.h"
 /*-----------------------------------------------------------------
 -                       Literal Constants
 -----------------------------------------------------------------*/
@@ -26,3 +26,30 @@
 /*-----------------------------------------------------------------
 -                          Procedures
 -----------------------------------------------------------------*/
+void audio_driver()
+{
+AudioFile<double> audioFile;
+audioFile.load ("~/Downloads/Abba - Dancing Queen.wav");
+
+int sampleRate = audioFile.getSampleRate();
+int bitDepth = audioFile.getBitDepth();
+
+int numSamples = audioFile.getNumSamplesPerChannel();
+double lengthInSeconds = audioFile.getLengthInSeconds();
+
+int numChannels = audioFile.getNumChannels();
+bool isMono = audioFile.isMono();
+bool isStereo = audioFile.isStereo();
+
+// or, just use this quick shortcut to print a summary to the console
+audioFile.printSummary();
+
+int channel = 0;
+int numSamples = audioFile.getNumSamplesPerChannel();
+
+for (int i = 0; i < numSamples; i++)
+{
+	double currentSample = audioFile.samples[channel][i];
+}
+
+}
